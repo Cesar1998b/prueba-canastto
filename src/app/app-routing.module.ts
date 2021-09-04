ab16a64ec5bba1bd8b713d8dd0a19fcad85693c2
Cesar David Botina Chacon <cdbc9082@coomeva.nal>
2021-09-03 22:18:00 -0500

    add products route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path:'', redirectTo:'/login', pathMatch:'full'},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'products', component:ProductsComponent}
 ];
 
 @NgModule({
