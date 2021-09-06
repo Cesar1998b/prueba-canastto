e009eed8e391c46005db0ba44c8ef9a720ca4997
Cesar David Botina Chacon <cdbc9082@coomeva.nal>
2021-09-05 20:10:52 -0500

    Se indexa el arreglo de categorias, se agregan los productos a las categorias en especifico ya ordenados y se realiza un flat para tener solo un for en el componente products

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
+})
+export class CategoriesComponent implements OnInit {
+  @Input() products: []; // Productos
+  categories = [];
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.productService
+      .getCategoriesAndProducts()
+      .subscribe((response: any) => {
+        this.categories = response.categories;
+        this.sortCategories(this.categories);
+        this.categories = this.addProductsInCategories(this.indexCategory(this.categories),this.products);
+      });
+  }
+
+  sortCategories(arr) {
+    arr.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (a.id < b.id) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  addProductsInCategories(arr, arr2) {
+    this.getCategories();
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        if (
+          arr2[j]['product_data']['categories'][0]['category_id'] ==
+          arr[i][1]['id']
+        ) {
+          arr[i].push(arr2[j]['product_data']);
+        }
+      }
+    }
+    let a = this.flatten(arr);
+    for (let i = 0; i < a.length; i++) {
+      if(a[i] == 0 || a[i] == 1 || a[i] == 2){
+        a.splice(i,1);
+      }
+    }
+    return a;
+  }
+
+  indexCategory(arr) {
+    var result = Object.keys(arr).map((key) => [Number(key), arr[key]]);
+    return result;
+  }
+
+  flatten(input) {
+    const stack = [...input];
+    const res = [];
+    while (stack.length) {
+      // elimina ultimo valor del stack
+      const next = stack.pop();
+      if (Array.isArray(next)) {
+        // agrega de nuevo los items al array, sin modificar la entrada original
+        stack.push(...next);
+      } else {
+        res.push(next);
+      }
+    }
+    //invierte para restaurar el orden de entrada
+    return res.reverse();
+  }
+}
