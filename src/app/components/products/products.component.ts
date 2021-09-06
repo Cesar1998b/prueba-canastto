1bf947a8850c22f341e2886b268bed87bfa665f6
Cesar David Botina Chacon <cdbc9082@coomeva.nal>
2021-09-05 20:12:28 -0500

    Se creo y modifico el componente products que obtiene el arreglo categories a través de un Input ya listo para usarse en el diseño de la página

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  @Input() categories: []; // Categorias de los productos
+  ready = false; //Boolean que nos notifica que categories esta listo para ser usado
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if(this.categories.length != 0){
+      this.ready = true;
+      console.log(this.categories);
+    }
   }
 
 }
+
